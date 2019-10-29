317fb05a46b7bfbe3bef00f2aed7b8297fdc32fd
Micha Alt <mail@lean-stack.de>
2019-10-29 20:09:45 +0100

    ng: todos shell component created and exported     ng g c todos/components/todo-shell --prefix --export

--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TodoShellComponent } from './components/todo-shell/todo-shell.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [TodoShellComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [TodoShellComponent]
 })
 export class TodosModule { }
