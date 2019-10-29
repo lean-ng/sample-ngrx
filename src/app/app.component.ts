af355d25e124facd247829d86ea688ad15cf26bc
Micha Alt <mail@lean-stack.de>
2019-10-29 21:01:06 +0100

    Starting with the footer line.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<todo-shell></todo-shell>'
+  template: `
+    <div class="flex-1">
+      <todo-shell></todo-shell>
+    </div>
+    <footer class="w-full text-center border-t border-grey p-4 pin-b">
+      {{ footerLine }}
+    </footer>
+  `,
+
 })
 export class AppComponent {
+  footerLine = 'Project: sample-ngrx';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
 import { TodosModule } from './todos/todos.module';
+
 
 @NgModule({
   declarations: [
